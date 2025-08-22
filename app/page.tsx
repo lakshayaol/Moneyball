5d95d33acaadaf04677a661c01ba7fd607e85d78
Lakshay Aol <lakshay05.aol@gmail.com>
2025-08-22 22:45:27 +0530

    Guest User Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
+import { currentUser } from '@clerk/nextjs/server'
+import Guest from '@/components/Guest'
 
-const page = () => {
+export default async function HomePage() {
+  const user = await currentUser()
+
+  if(!user) {
+    return <Guest />
+  }
+
   return (
     <div>
-      Home Page
+      <h1>Welcome to the Home Page</h1>
+      {user ? (
+        <p>Hello, {user.firstName} {user.lastName}!</p>
+      ) : (
+        <p>Please log in to see your profile.</p>
+      )}
     </div>
   )
 }
-
-export default page
